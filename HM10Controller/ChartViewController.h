982d3a147b91b0af9f2bd096e10a6db8f70239ec
Алексей Рогов <Dr.Morg@gmail.com>
2016-06-02 17:31:25 +0300

    Connect Charts framework Change target iOS to 8.0

---
+++ HM10Controller/ChartViewController.h
@@ -0,0 +1,13 @@
+//
+//  ChartViewController.h
+//  HM10Controller
+//
+//  Created by Dr. Morg on 02.06.16.
+//  Copyright © 2016 Alexey Rogov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ChartViewController : UIViewController
+
+@end
